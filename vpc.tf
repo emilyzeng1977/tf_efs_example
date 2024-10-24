f732a4003dd2e1eb9edac8ce05ec4b44c198f4fd
tom <tniu@infomedia.com.au>
2024-10-24 22:28:19 +1100

    efs

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true  # 启用 DNS 支持
+  enable_dns_hostnames = true  # 启用 DNS 主机名
 
   tags = {
     Name = "main_vpc"
